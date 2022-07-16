c0b142728160d9365f009547be0ab357d49d5826
Omar Elsayed <omar.alsayed@syscodeia.ae>
2022-07-16 21:02:15 +0200

    ci(core): :green_heart: add & update ci/cd workflows actions and dockerize apps

---
+++ Dockerfile
@@ -0,0 +1,40 @@
+ARG stage=prod
+ARG app
+
+# STAGE: Build Application
+FROM node:16.10.0-alpine3.14 AS build
+WORKDIR /usr/src/app
+COPY . .
+RUN npm i -g npm@latest
+RUN npm i
+RUN npx nx run-many --target=build --all --configuration=${stage} --parallel=false --source-map=true
+RUN export PROJECT=m-admin && npx ionic cap sync --prod --no-build
+
+# STAGE: Build Application
+FROM nginx:latest AS dist
+WORKDIR /usr/src/app
+COPY --from=build /usr/src/app/dist ./
+COPY --from=build /usr/src/app/android ./android
+COPY --from=build /usr/src/app/ios ./ios
+
+FROM nitrosystem/${stage}_build:latest AS distImg
+
+# Stage: Start Frontend Application
+FROM nginx:latest AS frontend
+ENV PORT=80
+COPY ./nginx.conf  /etc/nginx/conf.d/default.conf
+WORKDIR /usr/share/nginx/html
+RUN rm -rf ./*
+COPY --from=distImg /usr/src/app/apps/${app} .
+CMD sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+EXPOSE ${PORT}
+
+# Stage: Start Backend Application
+FROM node:16.10.0-alpine3.14 AS backend
+ENV PORT=3000
+WORKDIR /usr/src/app
+COPY --from=distImg /usr/src/app/apps/${app} .
+RUN npm i -g npm@latest
+RUN npm install --omit=dev
+CMD node main --bind 0.0.0.0:${PORT}
+EXPOSE ${PORT}
---
+++ docker.ps1
@@ -0,0 +1,28 @@
+﻿# 1- Build AND Push Apps For Production
+Docker build --tag nitrosystem/production_build:latest . --target=dist --build-arg stage=production
+Docker push nitrosystem/production_build
+
+# 2- Run Backend Api App Image
+Docker build --tag nitrosystem/production_b_api:latest . --target=backend --build-arg stage=production --build-arg app=b-api
+Docker push nitrosystem/production_b_api
+
+# 3- Run Web Admin App Image
+Docker build --tag nitrosystem/production_w_admin:latest . --target=frontend --build-arg stage=production --build-arg app=w-admin
+Docker push nitrosystem/production_w_admin
+
+# 4- Run Mobile Admin App Image
+Docker build --tag nitrosystem/production_m_admin:latest . --target=frontend --build-arg stage=production --build-arg app=m-admin
+Docker push nitrosystem/production_m_admin
+
+## Heroku app
+Docker tag nitrosystem/production_b_api registry.heroku.com/ns-production-b-api/web
+Docker push registry.heroku.com/ns-production-b-api/web
+heroku container:release web --app ns-production-b-api
+
+Docker tag nitrosystem/production_w_admin registry.heroku.com/ns-production-w-admin/web
+Docker push registry.heroku.com/ns-production-w-admin/web
+heroku container:release web --app ns-production-w-admin
+
+Docker tag nitrosystem/production_m_admin registry.heroku.com/ns-production-m-admin/web
+Docker push registry.heroku.com/ns-production-m-admin/web
+heroku container:release web --app ns-production-m-admin
